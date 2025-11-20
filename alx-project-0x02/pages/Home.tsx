6b435f5ead91ad4fe0ac44773ab98c568fddd8f1
Talent-1 <itzmhiztatalent@gmail.com>
2025-11-20 21:47:50 +0100

    add pages/home.tsx and pages/about.tsx files

--- alx-project-0x02/pages/Home.tsx
+++ alx-project-0x02/pages/Home.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Home: React.FC = () => {
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-4xl font-bold mb-4">Welcome to Daily Contents</h1>
+            <h1 className="text-4xl font-bold mb-4">Welcome </h1>
         </div>
     );
 }
