f9ecdc7f569d5b6e6621b2122145774283cb760d
Talent-1 <itzmhiztatalent@gmail.com>
2025-11-20 22:17:35 +0100

    create CardProps and components

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import Card from '@/components/common/Card'
 
 const Home: React.FC = () => {
     return (
         <div className="container mx-auto px-4 py-8">
             <h1 className="text-4xl font-bold mb-4">Welcome </h1>
+
+           <Card 
+           title="Daily Contents"
+           content="Your daily dose of insightful articles, tutorials, and stories to keep you informed and inspired."
+           />    
         </div>
     );
 }
